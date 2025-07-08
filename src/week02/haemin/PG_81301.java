154d190b410273c3ecd84dd42b52e0113dd77ba1
haemin4738 <132271194+haemin4738@users.noreply.github.com>
2025-07-08 09:48:03 +0900

    feat: 2주차 - PG_81301숫자 문자열과 영단어 문제 풀이 [이해민]

---
+++ src/week02/haemin/PG_81301.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+# 1
+class Solution {
+    
+    private static HashMap<String, String> map = new HashMap<>();
+    
+    public int solution(String s) {
+        // map 구성
+        map.put("zero", "0");
+        map.put("one", "1");
+        map.put("two", "2");
+        map.put("three", "3");
+        map.put("four", "4");
+        map.put("five", "5");
+        map.put("six", "6");
+        map.put("seven", "7");
+        map.put("eight", "8");
+        map.put("nine", "9");
+        
+        for(String key : map.keySet()){
+            s = s.replaceAll(key, map.get(key));
+        }
+
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
+
+# 2
+class Solution {
+    public int solution(String s) {
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
+        }
+        return Integer.parseInt(s);
+    }
+}
